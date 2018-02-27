38ea5095d6ea74193ea3b9f5ebd932de2216b67d
Paulo Salvatore <salvatoregames@gmail.com>
2018-02-26 22:40:08 -0300

    Initial commit

---
+++ Assets/Scripts/GeradorPedras.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorPedras : MonoBehaviour {
+
+	public GameObject pedrasPrefab;
+
+	public float intervaloPedras = 3f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CriarPedra", intervaloPedras, intervaloPedras);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CriarPedra() {
+		Instantiate (pedrasPrefab);
+	}
+}
---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Jogador : MonoBehaviour {
+
+	public float forcaPulo = 100f;
+
+	private int pontos = 0;
+
+	private Rigidbody rb;
+
+	public Text pontosText;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Pular ();
+		}
+	}
+
+	void Pular() {
+		rb.velocity = Vector3.zero;
+
+		rb.AddForce (Vector3.up * forcaPulo);
+	}
+
+	void OnCollisionEnter(Collision colisao) {
+		if (colisao.gameObject.CompareTag("Inimigos")) {
+			Invoke ("ReiniciarJogo", 2f);
+		}
+	}
+
+	void ReiniciarJogo() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.CompareTag ("ZonaPontos")) {
+			pontos = pontos + 1;
+
+			pontosText.text = "Pontos: " + pontos;
+		}
+	}
+}
